26c02ef57412a7e4a66c8d17d28473071ae68a6e
kevinchen8621 <103777673@qq.com>
2014-10-14 08:15:03 +0800

    a

--- controller.js
+++ controller.js
@@ -1,40 +1,4 @@
-'use strict';
-
-mo.config(['$stateProvider', '$urlRouterProvider','$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
-	$urlRouterProvider.otherwise("/");
-	$stateProvider
-		.state("f", {abstract: true, templateUrl: 'tpl/f/layout', controller:"FCtl"})
-		.state("f.home", {url:'/',  templateUrl: 'tpl/f/home', controller:"FHomeCtl"})
-		.state("f.cat", {url:'/cat/:id', templateUrl: 'tpl/f/cat',controller:'FCatCtl'})
-		.state("f.video", {url:'/video/:id', templateUrl: 'tpl/f/video',controller:'FVideoCtl'})
-		.state("s", {abstract:true, url:'/s',  templateUrl: 'tpl/s/layout', controller:"SignCtl"})
-		.state("s.signin", {url:'/signin',  templateUrl: 'tpl/s/signin', controller:"SignCtl"})
-		.state("s.signup", {url:'/signup',  templateUrl: 'tpl/s/signup', controller:"SignCtl"})
-		.state("s.forgot", {url:'/forgot',  templateUrl: 'tpl/s/forgot', controller:"SignCtl"})
-		.state("b", {abstract:true, url:'/b',  templateUrl: 'tpl/b/layout', controller:"BCtl"})
-		.state("b.home", {url:'/home',  templateUrl: 'tpl/b/home', controller:"BHomeCtl"})
-		.state("b.faq", {abstract:true, url:'/faq',  templateUrl: 'tpl/b/faq', controller:"BFaqCtl"})
-		.state("b.faq.home", {url:'/home',  templateUrl: 'tpl/b/faq.home', controller:"BFaqCtl"})
-		.state("b.faq.ask", {url:'/ask',  templateUrl: 'tpl/b/faq.ask', controller:"BFaqCtl"})
-		.state("b.faq.answer", {url:'/answer',  templateUrl: 'tpl/b/faq.answer', controller:"BFaqCtl"})
-		.state("b.account", {url:'/account',  templateUrl: 'tpl/b/account', controller:"BAccountCtl"})
-		.state("b.profile", {url:'/profile',  templateUrl: 'tpl/b/profile', controller:"BProfileCtl"})
-		.state("b.uppwd", {url:'/uppwd',  templateUrl: 'tpl/b/uppwd', controller:"BUppwdCtl"})
-		.state("b.getvip", {url:'/getvip',  templateUrl: 'tpl/b/getvip', controller:"BGetvipCtl"})
-		.state("b.buyvip", {url:'/buyvip',  templateUrl: 'tpl/b/buyvip', controller:"BBuyvipCtl"})
-		.state("b.gcode", {url:'/gcode',  templateUrl: 'tpl/b/gcode', controller:"BGcodeCtl"})
-		.state("b.data", {abstract:true, url:'/data',  templateUrl: 'tpl/b/data', controller:"BDataCtl"})
-		.state("b.data.finance", {url:'/finance',  templateUrl: 'tpl/b/data.finance', controller:"BDataCtl"})
-		.state("b.data.member", {url:'/member',  templateUrl: 'tpl/b/data.member', controller:"BDataCtl"})
-		.state("b.data.promotion", {url:'/promotion',  templateUrl: 'tpl/b/data.promotion', controller:"BDataCtl"})
-		.state("b.site", {abstract:true, url:'/site',  templateUrl: 'tpl/b/site', controller:"BSiteCtl"})
-		.state("b.site.home", {url:'/home',  templateUrl: 'tpl/b/site.home', controller:"BSiteCtl"})
-		.state("b.site.cat", {url:'/cat',  templateUrl: 'tpl/b/site.cat', controller:"BSiteCtl"})
-		.state("b.site.video", {url:'/video',  templateUrl: 'tpl/b/site.video', controller:"BSiteCtl"})
-		.state("b.site.d3", {url:'/d3',  templateUrl: 'tpl/b/site.d3', controller:"BSiteCtl"})
-	$httpProvider.interceptors.push('authInterceptor');
-}])
-.controller("AppCtl",["$scope", "$state", function($scope, $state){
+mo.controller("AppCtl",["$scope", "$state", function($scope, $state){
 	$state.transitionTo('f.home');	
 }])
 .controller("FCtl",["$scope","$rootScope","$http","$window", function($scope,$rootScope,$http,$window){
@@ -52,7 +16,7 @@ mo.config(['$stateProvider', '$urlRouterProvider','$httpProvider', function ($st
 				currentTimeout = $timeout(function(){
 					if (isPlaying) { 
 						console.log("here");
-						var newIndex = $rootScope.slides ?  (currentIndex + 1) % $rootScope.slides.length : 1;
+						var newIndex = (currentIndex + 1) % $rootScope.slides.length;
 						$scope.select(newIndex);
 					} else if (!$scope.noPause) { 
 						console.log("here2");
@@ -93,6 +57,48 @@ mo.config(['$stateProvider', '$urlRouterProvider','$httpProvider', function ($st
 			$scope.curVideoList = [$scope.curVideo];
 			servVideo.visit($stateParams.id);
 		}); 
+	}
+}])
+.controller("SignCtl",["$scope","$rootScope","$window","$state","servUser", function($scope,$rootScope,$window,$state,servUser){
+	if($rootScope.user){$state.go("b.home");}
+	$scope._id="";
+	$scope.username="";
+	$scope.password="";
+	$scope.sms="";
+	$scope.smsCaption="发送验证码";
+	$scope.smsDisable = false;
+	$scope.interval = 60;
+	$scope.rememberme = false;
+
+	$scope.sendSms = function(){
+		servUser.sendSignSms($scope._id, "钱路书院", 60, function(err, left){
+			$scope.smsCaption = left <= 0 ? "发送验证码" : left + " 秒后再次发送";
+		});
+	};
+	$scope.signup = function(){
+		var data = {_id:$scope._id, username:$scope.username, sms:$scope.sms, password:$scope.password};
+		servUser.signUp(data, function(err, msg){
+			$scope.msg = msg;
+			if(!msg){
+				$state.go("b.home");
+			}
+		});
+	};
+	$scope.signin = function(){
+		servUser.signIn($scope.username, $scope.password, $scope.rememberme, function(err, msg){
+			$scope.msg = msg;
+			if(!msg){
+				$state.go("b.home");
+			}
+		});
+	}
+	$scope.forget = function(){
+		servUser.forgot($scope.username, $scope.password, $scope.sms, function(err, msg){
+			$scope.msg = msg;
+			if(!msg){
+				$state.go("b.home");
+			}
+		});
 	}
 }])
 .controller("BCtl",["$scope", "$rootScope","$state", function($scope,$rootScope,$state){
@@ -244,101 +250,4 @@ mo.config(['$stateProvider', '$urlRouterProvider','$httpProvider', function ($st
 	$scope.addSubCat();
 	$scope.editCat();
 }])
-.controller("SignCtl", function($scope,$rootScope,$modalInstance,$state,servUser,servSms){
-	$scope.mdData = {
-		org: "钱路书院",
-		show: "", //显示哪个Modal
-		mobile:"",
-		email:"",
-		sms:"",
-		username:"",
-		password:"",
-		smsCaption: "发送验证码",
-		smsDisable: false,
-		smsInterval: 60,
-		rememberme: true,
-		agree: true,
-		msg: "",
-	};
-	$scope.sendSms = function(){
-		servSms.verify($scope.mdData, function(left){$scope.smsCaption = left <= 0 ? "发送验证码" : left + " 秒后再次发送";});
-	};
-	$scope.signin = function(){
-		servUser.signin($scope.mdData, function(msg){$modalInstance.close();});
-	};
-	$scope.signup_simple = function(){
-		servUser.signup_simple($scope.mdData, function(user){$modalInstance.close();});
-	};
-	$scope.signup_by_mobile = function(){
-		servUser.signup_by_mobile($scope.mdData, function(user){$modalInstance.close();});
-	};
-	$scope.signup_by_email = function(){
-		servUser.signup_by_email($scope.mdData, function(user){$modalInstance.close();});
-	};
-})
-.run(['$rootScope', '$location', '$modal', 'servUser', 'servSite', 'constants', function ($rootScope, $location, $modal, servUser,servSite,constants) {
-	console.log("here");
-	servUser.init();
-	servSite.init();
-	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-		$rootScope.toState = toState;
-		$rootScope.toParams = toParams;
-		$rootScope.fromState = fromState;
-		$rootScope.fromParams = fromParams;
-		$rootScope.bodycss = $rootScope.toState.name.substr(0,2) == 's.' ? "login-layout" : "";
-	});
-
-	$rootScope.sign = function(type){
-		var modalInstance = $modal.open({
-			templateUrl: "modal/"+type+".html",
-			controller: "SignCtl",
-			size: null,
-		});
-		modalInstance.result.then(function(){
-
-		})
-	};
-
-	$rootScope.toggleModal = function(key){
-		$rootScope.mdData.msg = "";
-		$rootScope.mdData.show = key;
-		console.log($rootScope.mdData);
-	}
-
-	$rootScope.sendSignSms = function(){
-		servUser.sendSignSms($rootScope.mdData, function(err, left){
-			$rootScope.mdData.smsCaption = left <= 0 ? "发送验证码" : left + " 秒后再次发送";
-		});
-	};
-	$rootScope.doSignup = function(){
-		servUser.signUp($rootScope.mdData, function(err, msg){
-			$scope.msg = msg;
-			if(!msg){
-				$rootScope.mdData.show = "";
-				$state.go("b.home");
-				$rootScope.toggleModal("");
-			}
-		});
-	};
-	$rootScope.doSignin = function(){
-		servUser.signIn($rootScope.mdData, function(err, msg){
-			$scope.msg = msg;
-			if(!msg){
-				$rootScope.mdData.show = "";
-				$state.go("b.home");
-				$rootScope.toggleModal("");
-			}
-		});
-	}
-	$rootScope.doForgotByEmail = function(){
-		servUser.forgot($rootScope.mdData, function(err, msg){
-			$scope.msg = msg;
-			if(!msg){
-				$rootScope.mdData.show = "";
-				$state.go("b.home");
-				$rootScope.toggleModal("");
-			}
-		});
-	}
-
-}]);
+;
